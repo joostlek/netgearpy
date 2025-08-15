05cb066eb355c25b132524d15d6be6a42b23f4f6
Joostlek <joostlek@outlook.com>
2025-08-15 21:52:53 +0200

    Add method to login

---
+++ src/netgearpy/const.py
@@ -0,0 +1,16 @@
+"""Constants for NetgearPy."""
+
+ENVELOPE = """<!--?xml version="1.0" encoding= "UTF-8" ?-->
+<v:Envelope
+xmlns:v="http://schemas.xmlsoap.org/soap/envelope/">
+<v:Header>
+<SessionID>A7D88AE69687E58D9A00</SessionID>
+</v:Header>
+<v:Body>
+{}
+</v:Body>
+</v:Envelope>"""
+
+
+LOGIN_BODY = """<M1:SOAPLogin xmlns:M1="urn:NETGEAR-ROUTER:service:DeviceConfig:1">
+<Username>{}</Username><Password>{}</Password></M1:SOAPLogin>"""
